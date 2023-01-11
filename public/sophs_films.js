0c059ae9d7ca9680604e975c08d309de37d8ba32
Sophia M Banda <51414936+sophiabanda@users.noreply.github.com>
2023-01-10 19:45:45 -0500

    created sophs_films.js

---
+++ public/sophs_films.js
@@ -0,0 +1,38 @@
+let topFilms = [
+
+  {
+    title: 'Goodfellas',
+    director: 'Martin Scorcese'
+  },
+
+  {
+    title: 'Jackie Brown',
+    director: 'Quentin Tarantino'
+  },
+
+  {
+    title: 'Casino',
+    director: 'Martin Scorcese'
+  },
+
+  {
+    title: 'Punch Drunk Love',
+    director: 'Paul Thomas Anderson'
+  },
+
+  {
+    title: 'Magnolia',
+    director: 'Paul Thomas Anderson'
+  },
+
+  {
+    title: '2001: A Space Odyssey',
+    director: 'Stanley Kubrick'
+  },
+
+  {
+    title: 'True Romance',
+    director: 'Tony Scott'
+  },
+
+];
