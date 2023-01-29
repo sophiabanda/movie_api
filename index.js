b9d26cba562690faf873785c23d8ecf24c1c1fd7
Sophia M Banda <51414936+sophiabanda@users.noreply.github.com>
2023-01-29 17:09:53 -0500

    working end points and udpated docs, minus add and remove film to favorites

--- index.js
+++ index.js
@@ -19,8 +19,8 @@ const mongoose = require('mongoose');
 
 const Films = Models.Film;
 const Users = Models.User;
-const Director = Models.Director;
-const Genre = Models.Genre;
+const Directors = Models.Director;
+const Genres = Models.Genre;
 
 mongoose.connect('mongodb://localhost:27017/sophiaFilms', { useNewUrlParser: true, useUnifiedTopology: true })
 
@@ -42,10 +42,94 @@ app.use(morgan('common'));
 app.use(morgan('combined', {stream: accessLogStream}));
 
 
+//---------------------------------------------------SITE
+//Homepage Welcome
 app.get('/', (req, res) => {
   res.status(201).send('Welcome to my film database!')
 })
-//Create new User
+
+//sets directory from which to grab static files:
+app.use(express.static('public'));
+
+//get retrieves all that is requested below:
+app.get('/documentation', (req, res) => {
+  res.sendFile('public/documentation.html', {root: __dirname});
+});
+
+//---------------------------------------------------FILMS
+//Get list of films
+app.get('/films', (req, res) => {
+  Films.find()
+  .then((films) => {
+    res.status(201).json(films);
+  })
+  .catch((err) => {
+    console.log(err);
+    res.status(500).send('Error: ' + err);
+  });
+});
+
+//Get Film by Title
+app.get('/films/:Title', (req, res) => {
+  Films.findOne( {Title: req.params.Title} )
+  .then((film) => {
+    res.status(201).json(film)
+  })
+  .catch((err) => {
+    res.status(500).send('Error ' + err);
+  });
+});
+
+//Get film by Genre type
+app.get('/films/genre/:genreType', (req, res) => {
+  //First retrieve the Genre doc with the matching Type:
+  Genres.findOne({ Type: req.params.genreType })
+  //Then retrrieve Films with matching genre id:
+   .then((genre) => {
+    Films.find({ Genres: genre._id })
+    .then((films) => {
+      res.status(200).json(films)
+    })
+    .catch((err) => {
+      res.status(500).send('Error ' + err);
+     });
+    })
+  });
+
+//Return all films by a particlar Director
+app.get('/films/director/:directorName', (req, res) => {
+  // First retrieve the Director doc with the matching name:
+  Directors.findOne({ Name: req.params.directorName })
+    .then((director) => {
+      // Then retrieve all Films with matching director id:
+      Films.find({ Director: director._id })
+        .then((films) => {
+          res.status(200).json(films);
+        })
+        .catch((err) => {
+          res.status(500).send(`Error: ${err}`);
+        });
+    })
+    ////Second error catch for the films search
+    .catch((err) => {
+      res.status(500).send(`Error: ${err}`);
+    });
+});
+
+//---------------------------------------------------USERS
+// Return all Users
+app.get('/users', (req, res) => {
+  Users.find()
+  .then((users) => {
+    res.status(201).json(users);
+  })
+  .catch((err) => {
+    console.log(err);
+    res.status(500).send('Error: ' + err);
+  });
+});
+
+//Create new User "Register"
 app.post('/users', (req, res) => {
   Users.findOne({ Name: req.body.Name })
     .then((user) => {
@@ -72,25 +156,6 @@ app.post('/users', (req, res) => {
     });
 });
 
-
-// app.get('/', (req, res) => {
-//   let responseMessage = "Welcome to a list of my favorite films.";
-//   responseMessage += '<small>Requested at:' + req.requestTimeStamp + '</small>';
-//   res.send(responseMessage);
-// });
-
-// Return all Users
-app.get('/users', (req, res) => {
-  Users.find()
-  .then((users) => {
-    res.status(201).json(users);
-  })
-  .catch((err) => {
-    console.log(err);
-    res.status(500).send('Error: ' + err);
-  });
-});
-
 // Find User by Name
 app.get('/users/:Name', (req, res) => {
   Users.findOne({ Name: req.params.Name })
@@ -106,6 +171,7 @@ app.get('/users/:Name', (req, res) => {
   });
 });
 
+//Find User by iD
 app.get('/users/id/:id', (req, res) => {
   Users.findOne( { _id: req.params.id} )
   .then((user) => {
@@ -120,65 +186,7 @@ app.get('/users/id/:id', (req, res) => {
   });
 });
 
-//Return list of films by Genre
-// app.get('/films/genre/:genreType', (req, res) => {
-//   Films.findOne({ Genre: req.params.genreType})
-//   .then((film) => {
-//     res.status(200).json(film)
-//   })
-//   .catch((err) => {
-//     res.status(500).send('Error ' + err);
-//   });
-// });
-//This is currently only displaying the first in the array of films. Even without the parameters in the object after findOne. Why  ? ? ? ?
-
-// app.get('/films/genre/:genreType', (req, res) => {
-//   Genre.findOne( { Genres: req.params.Genre} )
-//   .then((film) => {
-//     res.status(200).json(film)
-//   })
-//   .catch((err) => {
-//     res.status(500).send('Error ' + err);
-//   });
-// });
-//Returns the first genre type and desc of the array. Why?
-
-app.get('/films/genre/:genreType', (req, res) => {
-  //First retrieve the Genre doc with the matching Type:
-  Genre.findOne({ Type: req.params.genreType })
-  //Then retrrieve Films with matching genre id:
-   .then((genre) => {
-    Films.find({ Genres: genre._id })
-    .then((films) => {
-      res.status(200).json(films)
-    })
-    .catch((err) => {
-      res.status(500).send('Error ' + err);
-     });
-    })
-  });
-
-app.get('/films/director/:directorName', (req, res) => {
-  // First retrieve the Director doc with the matching name:
-  Director.findOne({ Name: req.params.directorName })
-    .then((director) => {
-      // Then retrieve all Films with matching director id:
-      Films.find({ Director: director._id })
-        .then((films) => {
-          res.status(200).json(films);
-        })
-        .catch((err) => {
-          res.status(500).send(`Error: ${err}`);
-        });
-    })
-    ////Second error catch for the films search
-    .catch((err) => {
-      res.status(500).send(`Error: ${err}`);
-    });
-});
-
-
-//Update User
+//Update User Info
 app.put('/users/:Name', (req, res) => {
   Users.findOneAndUpdate( {Name: req.params.Name}, { $set:
     {
@@ -200,73 +208,27 @@ app.put('/users/:Name', (req, res) => {
   });
 });
 
-//Update a Users Favorite Film
-app.post('/users/:Name/films/:filmID', (req,res) => {
-  Users.findOneAndUpdate( {Name: req.params.Name }, {
-    $push: { Favorites: req.params.filmID }
-  },
-  { new: true },
-  (err, updatedUser) => {
-    if(err) {
-      console.log(err);
-      res.status(500).send('Error ' + err);
-    } else {
-      res.json(updatedUser);
+
+//Add a Favorite Film to User's Favorites
+app.post('/users/:Username/films/:filmTitle', (req, res) => {
+  Users.findOneAndUpdate( { User: req.params.Username },
+    { $push: { Favorites: req.params.filmTitle } },
+    { new: true },
+    (err, udpatedUser) => {
+      if(err) {
+        console.log(err);
+        res.status(500).send('Error ' + err)
+      } else {
+        res.json(udpatedUser);
+      }
     }
-  }
-  );
+    );
 });
+//This still does not work for me so I did not move on to the delete version. 😭
 
-//Get list of films
-app.get('/films', (req, res) => {
-  Films.find()
-  .then((films) => {
-    res.status(201).json(films);
-  })
-  .catch((err) => {
-    console.log(err);
-    res.status(500).send('Error: ' + err);
-  });
-});
-
-//Get Film by Title
-app.get('/films/:Title', (req, res) => {
-  Films.findOne( {Title: req.params.Title} )
-  .then((film) => {
-    res.status(201).json(film)
-  })
-  .catch((err) => {
-    res.status(500).send('Error ' + err);
-  });
-});
-
-//Get list of directors
-app.get('/films/:directorName', (req, res) => {
-  Films.find( { Director: req.params.director})
-  .then((director) => {
-    res.status(201).json(director.name);
-  })
-  .catch((err) => {
-    console.log(err);
-    res.status(500).send('Error: ' + err);
-  });
-});
-
-// app.get('/secreturl', (req, res) => {
-//   res.send('This is a secret url with super top-secret content.');
-// });
-
-//sets directory from which to grab static files:
-app.use(express.static('public'));
-
-//get retrieves all that is requested below:
-app.get('/documentation', (req, res) => {
-  res.sendFile('public/documentation.html', {root: __dirname});
-});
-
-//Delete a User by username
+//Delete a User by Username "De-Register"
 app.delete('/users/:Username', (req, res) => {
-  Users.findOneAndRemove({ Name: req.params.Username }, { _id: req.params.userId})
+  Users.findOneAndRemove({ Name: req.params.Username })
     .then((user) => {
       if (!user) {
         res.status(400).send(req.params.Username + ' was not found');
@@ -280,6 +242,7 @@ app.delete('/users/:Username', (req, res) => {
     });
 });
 
+//Delete User by User iD
 app.delete('/users/id/:id', (req, res) => {
   Users.findOneAndRemove( {id: req.params.id })
     .then((user) => {
@@ -294,125 +257,7 @@ app.delete('/users/id/:id', (req, res) => {
       res.status(500).send('Error: ' + err);
     });
 });
-//READ
-// app.get('/sophs_films', (req, res) => {
-//   res.status(200).json(films);
-// });
 
-//READ
-// Get films by title
-// app.get('/sophs_films/:title', (req, res) => {
-//   const { title } = req.params;
-//   const film = films.find(film => film.Title.list === title);
-
-//   if (film) {
-//     res.status(200).json(film);
-//   } else {
-//     res.status(400).send('No such film.');
-//   }
-// });
-
-//READ
-//Return movie with specified genres
-// app.get('/sophs_films/genre/:genreType', (req, res) => {
-//   const { genreType } = req.params;
-//   const genre = films.filter(film => film.genre.includes(genreType));
-
-//   if (genre) {
-//   res.status(200).json(genre);
-//   } else {
-//   res.status(400).send('No such genre.');
-//   }
-//   });
-
-
-//READ
-//Get info on a particular director
-// app.get('/sophs_films/director/:directorName', (req, res) => {
-//   const { directorName } = req.params;
-//   const director = films.filter(film => film.director.name === directorName);
-
-//   if (director) {
-//     res.status(200).json(director);
-//   } else {
-//     res.status(400).send('No such director.');
-//   }
-// });
-
-//CREATE
-// Creates new user
-// app.post('/users', (req, res) => {
-//   const newUser = req.body;
-
-//   if(newUser.firstName || newUser.lastName) {
-//     newUser.idNum = uuid.v4();
-//     users.push(newUser);
-//     res.status(201).json(newUser)
-//   } else {
-//     res.status(400).send('Users need names.')
-//   };
-// });
-
-//UPDATE
-//Updates user name
-app.put('/users/:idNum', (req, res) => {
-  const {idNum } = req.params;
-  const udpatedUser = req.body;
-
-  let user = users.find(user => user.idNum == idNum);
-
-  if(user) {
-    user.firstName = udpatedUser.firstName;
-    res.status(200).json(user)
-  } else {
-    res.status(400).send('User not found.');
-  }
-});
-
-//UPDATE
-//Adds a fav to films
-app.put('/users/:idNum/:filmTitle', (req, res) => {
-  const { idNum, filmTitle } = req.params;
-
-  let user = users.find(user => user.idNum == idNum);
-
-  if(user) {
-    user.favoriteFilms.push(filmTitle);
-    res.status(200).send(`${filmTitle} has been added to user ${idNum}'s favorite films.`)
-  } else {
-    res.status(400).send('User not found.');
-  }
-});
-
-//DELETE
-//Deletes user fav film
-app.delete('/users/:idNum/:filmTitle', (req, res) => {
-  const { idNum, filmTitle } = req.params;
-
-  let user = users.find(user => user.idNum == idNum);
-
-  if(user) {
-    user.favoriteFilms = user.favoriteFilms.filter(title => title !== filmTitle);
-    res.status(200).send(`${filmTitle} has been removed from user ${idNum}'s favorite films.`)
-  } else {
-    res.status(400).send('User not found.');
-  }
-});
-
-//DELETE
-//Deletes user
-// app.delete('/users/:idNum', (req, res) => {
-//   const { idNum } = req.params;
-
-//   let user = users.find(user => user.idNum == idNum);
-
-//   if(user) {
-//     users = users.filter(user => user.idNum == idNum);
-//     res.status(200).send(`User ${idNum} has been deleted.`)
-//   } else {
-//     res.status(400).send('User not found.');
-//   }
-// });
 
 //returns "something broke!" if there is an error delivering on any of the above:
 app.use((err, req, res, next) => {
