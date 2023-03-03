9ee10627d541862bc6b8e3c39654993a1341ebd4
Sophia M Banda <51414936+sophiabanda@users.noreply.github.com>
2023-03-03 17:08:30 -0500

    changed update to id from name

--- index.js
+++ index.js
@@ -213,7 +213,7 @@ app.post('/users',
   });
 
   //UPDATE User Info ------
-  app.put('/users/:Name', passport.authenticate('jwt', {session: false}), //Is it safe to have access to change all those fields with just url and token access?
+  app.put('/users/:id', passport.authenticate('jwt', {session: false}), //Is it safe to have access to change all those fields with just url and token access?
   [check('Name', 'Name must be at least 5 alphanumeric characters')
     .optional( {nullable: true} ) //optional with 'nullable: true' parameter ensures that the sequence won't fail if the check is missing
     .isLength( {min: 5} )
@@ -238,7 +238,7 @@ app.post('/users',
 
     let hashedPassword = Users.hashPassword(req.body.Password);
     //req params traditionally lowercase
-    Users.findOneAndUpdate( {Name: req.params.Name}, { $set:
+    Users.findOneAndUpdate( {id: req.params.id}, { $set:
       {
         Name: req.body.Name,
         Email: req.body.Email,
