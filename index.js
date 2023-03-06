ca8366e1936c37d42296e216e164c5de7c072448
Sophia M Banda <51414936+sophiabanda@users.noreply.github.com>
2023-03-06 15:32:49 -0500

    put to patch

--- index.js
+++ index.js
@@ -213,7 +213,7 @@ app.post('/users',
   });
 
   //UPDATE User Info ------
-  app.put('/user/id/:id', passport.authenticate('jwt', {session: false}), //Is it safe to have access to change all those fields with just url and token access?
+  app.patch('/user/id/:id', passport.authenticate('jwt', {session: false}), //Is it safe to have access to change all those fields with just url and token access?
   [check('Name', 'Name must be at least 5 alphanumeric characters')
     .optional( {nullable: true} ) //optional with 'nullable: true' parameter ensures that the sequence won't fail if the check is missing
     .isLength( {min: 5} )
