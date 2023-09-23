27acda0a4c6dff6f4ff33fa3a7172c6d82778fc0
Sophia M Banda <51414936+sophiabanda@users.noreply.github.com>
2023-09-23 14:31:42 -0400

    cors

--- index.js
+++ index.js
@@ -62,8 +62,6 @@ let allowedOrigins = [
   "http://localhost:8080",
   "http://localhost:1234",
   "https://select-films.netlify.app/",
-  "https://select-films.netlify.app/login",
-  "https://select-films.netlify.app/profile",
 ];
 
 app.use(
